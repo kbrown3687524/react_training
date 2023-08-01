0d3260945a2de7222a026e3af53571c27e511787
Keaghan Brown <3687524@myuwc.ac.za>
2023-08-01 14:54:03 +0200

    "React training "

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-ReactDOM.render(<h1>Hello, everyone</h1>, document.getElementById("root"));
+const h1 = document.createElement("h1");
+h1.textContent = "this is an imperative way to program";
+h1.className = "header";
+document.getElementById("root").append(h1);
+console.log(h1);
+
+const element = <h1 className="header">This is JSX</h1>;
+console.log(element);
+
+ReactDOM.render(element, document.getElementById("root"));
