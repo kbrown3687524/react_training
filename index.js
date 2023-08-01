41d82c08a0744eb07a23a105345f01eb5e289e8b
Keaghan Brown <3687524@myuwc.ac.za>
2023-08-01 15:04:19 +0200

    "react tutorial  update"

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const h1 = document.createElement("h1");
-h1.textContent = "this is an imperative way to program";
-h1.className = "header";
-document.getElementById("root").append(h1);
-console.log(h1);
+// const h1 = document.createElement("h1");
+// h1.textContent = "this is an imperative way to program";
+// h1.className = "header";
+// document.getElementById("root").append(h1);
+// console.log(h1);
 
 const element = <h1 className="header">This is JSX</h1>;
 console.log(element);
